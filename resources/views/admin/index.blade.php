c332e4f847e7861d59a5d75864036555a679ba5f
PriyadiSentosa <priyadisentosa@gmail.com>
2022-02-14 11:37:49 +0700

    Projeck

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.admin')
+@section('header')
+<div class="content-headear">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-md-12">
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <center><div class="card-header"><b>Penjualan Alat Olahraga</b></div></center>
+                <div class="card-header">Admin Page</div>
+                <div class="card-body">
+                    Welcome back <strong>Admin</strong>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
